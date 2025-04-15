a8f958f64ea15e796f803a7b07263dfe3ff84c08
neyneyalldayday <drfrankenstein123@gmail.com>
2025-04-15 11:56:12 -0500

    routes working

--- src/routes/api/trade-routes.ts
+++ src/routes/api/trade-routes.ts
@@ -1,14 +1,16 @@
-import { Router } from 'express';
-const router = Router();
-import  { Request, Response } from 'express';
+import express, { Router, Request, Response } from 'express';
 import  Alpaca  from '@alpacahq/alpaca-trade-api';
 import { AlpacaBar } from '../../types.js'
+
 import 'dotenv/config';
+
+const router: Router = express.Router();
 
 const client = new Alpaca({
     keyId: process.env.ALPACA_KEY,
     secretKey: process.env.ALPACA_SECRET,
     paper: true,
+    verbose: true,
   });
   
   // Health check endpoint
@@ -33,24 +35,34 @@ const client = new Alpaca({
     }
   });
 
-  router.post('/webhook', async (req: Request, res: Response) => {
+  router.post('/webhook', async (req: Request, res: Response): Promise<void> => {  
+
+   try {
     const { symbol, action } = req.body; // e.g., { "symbol": "BTCUSD", "action": "buy" }
     
-    if (action === 'buy') {
-      await client.createOrder({
-        symbol,
-        qty: 1,
-        side: 'buy',
-        type: 'market',
-      });
-      res.json({ success: true });
-    } else {
-      res.status(400).json({ error: 'Invalid action' });
+    if (!symbol || !['buy', 'sell'].includes(action)) {
+      res.status(400).json({ error: 'Invalid symbol or action'});
+      return;
     }
+    const qty = symbol.endsWith('USD') ? 0.01 : 1;
+
+    await client.createOrder({
+        symbol: symbol.toUpperCase(),
+        qty,
+        side: action,
+        type: 'market',
+        time_in_force: 'gtc',
+    });
+
+    res.json({ success: true });
+    } catch (error) {
+        const err = error as { response?: {data?: any}, message: string};
+    console.error("Order error: ", err.response?.data || err.message)
+   }
   });
 
   async function getBarsArray(symbol: string): Promise<AlpacaBar[]> {
-    const bars = client.getBarsV2(symbol, { timeframe: 'iDay', limit: 1 });
+    const bars = client.getBarsV2(symbol, { timeframe: '1D', limit: 1 });
     const barArray: AlpacaBar[] = [];
 
 
@@ -62,10 +74,9 @@ const client = new Alpaca({
   }
   
   // Strategy logic (reusable)
-  async function checkSPY(symbol: string): Promise<void> {
-    console.log("hello?")
+  async function checkSPY(symbol: string): Promise<void> {  
     const barArray = await getBarsArray(symbol);  
-    console.log(barArray);  
+    console.log(barArray, "bar array");  
     const latestBar: any = barArray[0];
 
     if(!latestBar){
